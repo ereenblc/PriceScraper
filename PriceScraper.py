775f71abc3460071750bb5b2108c78fbdcceeea2
Eren Balcı <eren.balci2111@gmail.com>
2023-09-24 00:26:47 +0300

    trendyol & amazon

---
+++ PriceScraper.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.keys import Keys
+
+target_product = input("Which product you looking for? : ")
+
+
+class PriceScraper():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        
+    def amazon_list(self):
+        self.driver.get("https://www.amazon.com.tr/")
+        WebDriverWait(self.driver, 10).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, "nav-input")))
+        search_engine = self.driver.find_element(By.CLASS_NAME, "nav-input")
+        search_engine.send_keys(target_product)
+        search_engine.send_keys(Keys.ENTER)
+
+        product_names = self.driver.find_elements(By.CLASS_NAME, "a-size-base-plus")
+        product_prices = self.driver.find_elements(By.CLASS_NAME, "a-price-whole")
+
+        product_data = []
+        for product_name, product_price in zip(product_names, product_prices):
+            product_data.append({
+                "Product Name": product_name.text,
+                "Price ": product_price.text
+            })
+        return product_data
+
+    def trendyol_list(self):
+        self.driver.get("https://www.trendyol.com/")
+        WebDriverWait(self.driver, 4).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, "V8wbcUhU")))
+        search_engine = self.driver.find_element(By.CLASS_NAME, "V8wbcUhU")
+        search_engine.send_keys(target_product)
+        search_engine.send_keys(Keys.ENTER)
+        WebDriverWait(self.driver, 10).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, "prdct-cntnr-wrppr")))
+
+        product_names = self.driver.find_elements(By.CLASS_NAME, "prdct-desc-cntnr-name")
+        product_prices = self.driver.find_elements(By.CLASS_NAME, "prc-box-dscntd")
+
+        product_data = []
+        for product_name, product_price in zip(product_names, product_prices):
+            product_data.append({
+                "Product Name": product_name.text,
+                "Price ": product_price.text
+            })
+        return product_data
+
--- main.py
+++ main.py
@@ -1,43 +1,15 @@
+from PriceScraper import PriceScraper
+import pandas as pd
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.keys import Keys
+scraper = PriceScraper()
 
-target_product = input("Which product you looking for?:")
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get("https://www.trendyol.com/")
+amazon_data = scraper.amazon_list()
+trendyol_data = scraper.trendyol_list()
 
-# searching the product you are looking for
-WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "V8wbcUhU")))
-search_engine = driver.find_element(By.CLASS_NAME, "V8wbcUhU")
-search_engine.send_keys(target_product)
-search_engine.send_keys(Keys.ENTER)
-WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "prdct-cntnr-wrppr")))
+amazon_df = pd.DataFrame(amazon_data)
+trendyol_df = pd.DataFrame(trendyol_data)
 
-# listing the top 10 prices of your product
-for i in range(2, 12):
-    name_path = (f"/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[4]/div[1]/div/div[{i}]/div["
-                 f"1]/a/div[2]/div[1]/div/h3/span[2]")
-    product_name = driver.find_element(By.XPATH, name_path)
-
-    price_path = (f"/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[4]/div[1]/div/div[{i}]"
-                  f"/div[1]/a/div[2]/div[3]/div/div")
-    product_price = driver.find_element(By.XPATH, price_path)
-
-    if "TL" in product_price.text:
-        print(f"\n*** Product {i - 1} ***"
-              f"\n{product_name.text}"
-              f"\n{product_price.text}")
-
-    else:
-        price_path = (f"/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[4]/div[1]/div/div[{i}]"
-                  f"/div[1]/a/div[2]/div[3]/div/div[2]/div")
-        product_price = driver.find_element(By.XPATH, price_path)
-        print(f"\n*** Product {i - 1} ***"
-              f"\n{product_name.text}"
-              f"\n{product_price.text}")
-
-driver.quit()
+print(f"------------------------TRENDYOL DATA-----------------------------"
+      f"\n{trendyol_df}\n"
+      f"\n------------------------AMAZON DATA-----------------------------"
+      f"\n{amazon_df}")
